e149565b42f7e16f37ef926c405e03887a2881ca
Divy9335 <landingrajat@gmail.com>
2025-09-05 22:51:34 +0530

    covid and theater problem

--- covidAndTheatreTickets.java
+++ covidAndTheatreTickets.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class covidAndTheatreTickets {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int noOfRows = sc.nextInt();
+            int noOfColumn = sc.nextInt();
+            int totalTicket = 0;
+            if(noOfColumn%2==0){
+                totalTicket+=noOfColumn/2;
+            }else{
+                totalTicket+=(noOfColumn/2)+1;
+            }
+            int eligibleRowSeats = 0;
+            if(noOfRows%2==0){
+                eligibleRowSeats+=noOfRows/2;
+            }else{
+                eligibleRowSeats+=(noOfRows/2)+1;
+            }
+            System.out.println(totalTicket*eligibleRowSeats);
+        }
+    }
 }
