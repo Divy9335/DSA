f23c17795644000403f5081ad49164778dfa4462
Divy9335 <landingrajat@gmail.com>
2025-09-09 00:25:14 +0530

    problem no. 1090

--- FarAway.java
+++ FarAway.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class FarAway {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int lengthOfArray = sc.nextInt();
+            int limitForAandB = sc.nextInt();
+            int arr [] = new int[lengthOfArray];
+            long ans =0;
+            for(int i=0;i<lengthOfArray;i++){
+                arr[i] = sc.nextInt();
+                ans+= Math.max(Math.abs(1-arr[i]),Math.abs(limitForAandB-arr[i]));
+
+            }
+            System.out.println(ans);
+
+
+        }
+    }
 }
